5857991ef26cc2187ca93d2f556485b7456c686a
Lindsay Lohan Teixeira de Oliveira <2022102020082@aluno.ifro.edu.br>
2023-11-10 15:39:31 -0400

    petcão

---
+++ po2PetShop/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+
+public class Funcionario
+
+{
+    public string name = "";
+    public string cpf;
+    public double rg;
+    public string email;
+    public string celular;
+    public string dataNasc;
+    public string estadoCiv;
+    public string endereco;
+    public string cidade;
+    public string estado;
+    public double salario;
+    public string funcao;
+
+    public string VerificaFunc( string funcao)
+    {
+        if ( funcao == "Administrador")
+        {
+            string valor = "10000";
+            return valor;
+        }
+    }
+
+}
